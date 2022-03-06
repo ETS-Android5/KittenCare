052100f47689fba590ef6e3589aabcaabbd7d655
Cluster Pleiades <Pleione.P.Cluster@gmail.com>
2022-03-06 19:00:03 +0900

    Upgrade Gradle Plugin

--- app/build/generated/source/buildConfig/debug/com/pleiades/pleione/kittencare/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/pleiades/pleione/kittencare/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.pleiades.pleione.kittencare";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 32;
-  public static final String VERSION_NAME = "1.9.1";
+  public static final int VERSION_CODE = 34;
+  public static final String VERSION_NAME = "2.0.0";
 }
